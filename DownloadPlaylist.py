2e439f04f98e5f3e183df9fbe672cb1955570f08
AGlinkowski <adrian.glinkowski@gmail.com>
2021-07-04 17:28:40 +0200

    Dodanie konwersji mp3 i usunięcia niepotrzebnego mp4

--- DownloadPlaylist.py
+++ DownloadPlaylist.py
@@ -1,16 +1,21 @@
 from pytube import Playlist
+from moviepy.editor import *
 import os
 
 url = input("Podaj ścieżkę do playlisty: ")
 p = Playlist(url)
-playlistName = p.title
+audioName = p.title
 playlistCount = len(p.video_urls)
-print("Pobieram playlistę " +  playlistName + " (" + str(playlistCount) + " utworów)")
+print("Pobieram playlistę " +  audioName + " (" + str(playlistCount) + " utworów)")
 it = 0
 
 for video in p.videos:
     it += 1
     print("Pobieram: " + str(it) + " z " + str(playlistCount))
-    dir = video.streams.get_audio_only().download(os.path.join(os.path.expanduser('~'), 'downloads\\') + playlistName)
+    dir = video.streams.get_audio_only().download(os.path.join(os.path.expanduser('~'), 'downloads\\') + audioName)
+    mp3 = AudioFileClip(dir)
+    mp3.write_audiofile(dir[:-4] + ".mp3")
+    mp3.close()
+    os.remove(dir)
     print(dir)
     
